5787d206530aaadcef07947f0f95ade45de02786
Jason Harris <lord.jtharris@gmail.com>
2024-08-06 15:32:25 -0500

    Adding a bit of baseline xml

---
+++ EasyTabardDesigner.lua
@@ -0,0 +1,5 @@
+-- Written by ToadNamedDuck/Jason Harris in (August) 2024
+-- Blizzard's current Tabard UI is terrible, and I want it to look a little more like the collections panel with each option showing up.
+
+-- When changing Icons, the right side panels should show a TabardModel for each available icon, each of which keeps the current border, border color, and background color.
+
