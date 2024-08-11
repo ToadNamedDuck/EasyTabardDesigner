32acb4b3fabc4e75a4c324efc4a9b415765f7ec9
Jason Harris <lord.jtharris@gmail.com>
2024-08-11 16:02:49 -0500

    implemented function that hopefully will get me icon IDs

--- EasyTabardDesigner.lua
+++ EasyTabardDesigner.lua
@@ -38,6 +38,7 @@ EasyTabardDesigner_Open = function()
     EasyTabardDesigner_TabardModel:InitializeTabardColors();
 end
 
+-- There are supposed to be 4 elements which hold a quadrant of the guild emblem. The param is the name of the quadrant, and updatetextures sets all 4 of those, and that should theoretically be in a frames overlay level.
 function EasyTabardDesigner_UpdateTextures()
 	EasyTabardDesigner_TabardModel:GetUpperEmblemTexture(TabardFrameEmblemTopLeft);
 	EasyTabardDesigner_TabardModel:GetUpperEmblemTexture(TabardFrameEmblemTopRight);
@@ -49,8 +50,40 @@ function EasyTabardDesigner_OnEvent(self, event, ...)
     if event == "UNIT_MODEL_CHANGED" then EasyTabardDesigner_TabardModel:SetUnit("player") end
 end
 
+-- Supposedly, every file has an ID, so in theory it is possible to get the name of the file or the id or something, and compile a list of the ids or names. I also kind of want to give them a label, which means naming all 196 icons.
+-- I wonder if I can do a get emblem texture from the functions above with a getfile or something
+function EasyTabardDesigner_GetTabardIconIds()
+    local tabardIconIDs = {};
+    local i = 1;
+    --Need to initialize tabard colors so we have a starting point.
+    EasyTabardDesigner_TabardModel:InitializeTabardColors();
+    local IconID = EasyTabardDesigner_TabardModel:GetUpperEmblemFileName(TabardFrameEmblemTopLeft)
+
+    while(true) do
+        if (not tabardIconIDs[i])
+        then
+            tabardIconIDs[i] = {ID = IconID, Name = ""};
+            EasyTabardDesignerTabardModel:CycleVariation(1,1);
+            i = i + 1;
+        else
+            break;
+        end
+    end
+
+    table.sort(tabardIconIDs, function(a,b) return a.ID < b.ID end)
+    return tabardIconIDs;
+end
+
 -- EasyTabardDesigner_IconSelectButton_Click = function(tabardIconId) ?
 -- EasyTabardDesigner_IconColorButton_Click = function(TabardIconColor) ?
 -- EasyTabardDesigner_BorderSelectButton_Click = function(BorderId) ?
 -- EasyTabardDesigner_BorderColorSelectButton_Click = function(BorderColor) ?
 -- EasyTabardDesigner_BackgroundColorSelectButton_Click = function(BackgroundColor) ?
+
+
+-- local object1 =  {ID = 01, IconName = ""};
+-- local object2 = {ID = 01, IconName = ""};
+-- local table = {};
+-- table[1] = object1;
+-- table[2] = object2;
+-- print(table[1].ID == table[2].ID);
