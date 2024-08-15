f13eb4262795db0b4405c4946b743b0eaa1fc7f2
Jason Harris <lord.jtharris@gmail.com>
2024-08-15 17:25:11 -0500

    Fixed some table members Id to ID

--- TabardIconIDs.lua
+++ TabardIconIDs.lua
@@ -296,687 +296,687 @@ EasyTabardDesigner_TabardTable = {
         RangeEnd = 184783
     },
     {
-        Id = 184784,
+        ID = 184784,
         Name = "Keyhole",
         RangeEnd = 184817
     },
     {
-        Id = 184818,
+        ID = 184818,
         Name = "Barren Tree",
         RangeEnd = 184851
     },
     {
-        Id = 184852,
+        ID = 184852,
         Name = "Shuriken 1",
         RangeEnd = 184885
     },
     {
-        Id = 184886,
+        ID = 184886,
         Name = "Shuriken 2",
         RangeEnd = 184919
     },
     {
-        Id = 184920,
+        ID = 184920,
         Name = "Downward Chevron",
         RangeEnd = 184953
     },
     {
-        Id = 184954,
+        ID = 184954,
         Name = "Upward Chevron",
         RangeEnd = 184987
     },
     {
-        Id = 184988,
+        ID = 184988,
         Name = "Roach",
         RangeEnd = 185021
     },
     {
-        Id = 185022,
+        ID = 185022,
         Name = "Haunch of Meat",
         RangeEnd = 185055
     },
     {
-        Id = 185056,
+        ID = 185056,
         Name = "Muffin",
         RangeEnd = 185089
     },
     {
-        Id = 185090,
+        ID = 185090,
         Name = "Bowl of Soup",
         RangeEnd = 185123
     },
     {
-        Id = 185124,
+        ID = 185124,
         Name = "Crying Bear",
         RangeEnd = 185157
     },
     {
-        Id = 185158,
+        ID = 185158,
         Name = "Ribcage",
         RangeEnd = 185191
     },
     {
-        Id = 185192,
+        ID = 185192,
         Name = "Scarecrow",
         RangeEnd = 185225
     },
     {
-        Id = 185226,
+        ID = 185226,
         Name = "Hand of Cards",
         RangeEnd = 185259
     },
     {
-        Id = 185260,
+        ID = 185260,
         Name = "Lobster",
         RangeEnd = 185293
     },
     {
-        Id = 185294,
+        ID = 185294,
         Name = "Flower",
         RangeEnd = 185327
     },
     {
-        Id = 185328,
+        ID = 185328,
         Name = "Mugs of Ale",
         RangeEnd = 185361
     },
     {
-        Id = 185362,
+        ID = 185362,
         Name = "Skeleton Key",
         RangeEnd = 185395
     },
     {
-        Id = 185396,
+        ID = 185396,
         Name = "Staring Cat",
         RangeEnd = 185429
     },
     {
-        Id = 185430,
+        ID = 185430,
         Name = "Monkey Face",
         RangeEnd = 185463
     },
     {
-        Id = 185464,
+        ID = 185464,
         Name = "Satisfied Cat",
         RangeEnd = 185197
     },
     {
-        Id = 185498,
+        ID = 185498,
         Name = "Squirrel",
         RangeEnd = 185531
     },
     {
-        Id = 185532,
+        ID = 185532,
         Name = "Wrench",
         RangeEnd = 185565
     },
     {
-        Id = 185566,
+        ID = 185566,
         Name = "Arcane Eye",
         RangeEnd = 185599
     },
     {
-        Id = 185600,
+        ID = 185600,
         Name = "Wheel with Spokes",
         RangeEnd = 185633
     },
     {
-        Id = 185634,
+        ID = 185634,
         Name = "Bat",
         RangeEnd = 185667
     },
     {
-        Id = 185668,
+        ID = 185668,
         Name = "Hourglass",
         RangeEnd = 185701
     },
     {
-        Id = 185702,
+        ID = 185702,
         Name = "Vial of Poison",
         RangeEnd = 185735
     },
     {
-        Id = 185736,
+        ID = 185736,
         Name = "Antler Skull",
         RangeEnd = 185769
     },
     {
-        Id = 185770,
+        ID = 185770,
         Name = "Acorn",
         RangeEnd = 185803
     },
     {
-        Id = 185804,
+        ID = 185804,
         Name = "Lightning Coil",
         RangeEnd = 185837
     },
     {
-        Id = 185838,
+        ID = 185838,
         Name = "Pile of Coins",
         RangeEnd = 185871
     },
     {
-        Id = 185872,
+        ID = 185872,
         Name = "Owl",
         RangeEnd = 185905
     },
     {
-        Id = 185906,
+        ID = 185906,
         Name = "Skull and Crossbones",
         RangeEnd = 185939
     },
     {
-        Id = 185940,
+        ID = 185940,
         Name = "Flaming Torch",
         RangeEnd = 185973
     },
     {
-        Id = 185974,
+        ID = 185974,
         Name = "Tombstone",
         RangeEnd = 186007
     },
     {
-        Id = 186008,
+        ID = 186008,
         Name = "Water Drop",
         RangeEnd = 186041
     },
     {
-        Id = 186042,
+        ID = 186042,
         Name = "Shining Diamond",
         RangeEnd = 186075
     },
     {
-        Id = 186076,
+        ID = 186076,
         Name = "Templar Cross",
         RangeEnd = 186109
     },
     {
-        Id = 186110,
+        ID = 186110,
         Name = "Infinity",
         RangeEnd = 186143
     },
     {
-        Id = 186144, -- 100
+        ID = 186144, -- 100
         Name = "Stars and Moon",
         RangeEnd = 186177
     },
     {
-        Id = 180738,
+        ID = 180738,
         Name = "Sunrise",
         RangeEnd = 180771
     },
     {
-        Id = 180772,
+        ID = 180772,
         Name = "Pawprint",
         RangeEnd = 180805
     },
     {
-        Id = 180806,
+        ID = 180806,
         Name = "Paw with Claws",
         RangeEnd = 180839
     },
     {
-        Id = 180840,
+        ID = 180840,
         Name = "Bloodshot Eyes",
         RangeEnd = 180873
     },
     {
-        Id = 180874,
+        ID = 180874,
         Name = "Target",
         RangeEnd = 180907
     },
     {
-        Id = 180908,
+        ID = 180908,
         Name = "Three Beings",
         RangeEnd = 180941
     },
     {
-        Id = 180942,
+        ID = 180942,
         Name = "Vampire Fangs",
         RangeEnd = 180975
     },
     {
-        Id = 180976,
+        ID = 180976,
         Name = "Rock On",
         RangeEnd = 181009
     },
     {
-        Id = 181010,
+        ID = 181010,
         Name = "Lightning Fist",
         RangeEnd = 181043
     },
     {
-        Id = 181044,
+        ID = 181044,
         Name = "Checkerboard",
         RangeEnd = 181077
     },
     {
-        Id = 181112,
+        ID = 181112,
         Name = "Maple Leaf",
         RangeEnd = 181145
     },
     {
-        Id = 181146,
+        ID = 181146,
         Name = "Shining Trident",
         RangeEnd = 181179
     },
     {
-        Id = 181180,
+        ID = 181180,
         Name = "Volcanic Eruption",
         RangeEnd = 181213
     },
     {
-        Id = 181214,
+        ID = 181214,
         Name = "Bull",
         RangeEnd = 181247
     },
     {
-        Id = 181248,
+        ID = 181248,
         Name = "Octopus",
         RangeEnd = 181281
     },
     {
-        Id = 181282,
+        ID = 181282,
         Name = "Tower",
         RangeEnd = 181315
     },
     {
-        Id = 181316,
+        ID = 181316,
         Name = "Crossed Hammers",
         RangeEnd = 181349
     },
     {
-        Id = 181350,
+        ID = 181350,
         Name = "Crossed Spears",
         RangeEnd = 181383
     },
     {
-        Id = 181384,
+        ID = 181384,
         Name = "Crossed Arrows",
         RangeEnd = 181417
     },
     {
-        Id = 181418, -- 120
+        ID = 181418, -- 120
         Name = "Crossed Swords",
         RangeEnd = 181451
     },
     {
-        Id = 181486,
+        ID = 181486,
         Name = "Crossed Axes",
         RangeEnd = 181519
     },
     {
-        Id = 181520,
+        ID = 181520,
         Name = "Crossed Scythes",
         RangeEnd = 181553
     },
     {
-        Id = 181554,
+        ID = 181554,
         Name = "Spiked Knuckles",
         RangeEnd = 181587
     },
     {
-        Id = 181588,
+        ID = 181588,
         Name = "Claw Mitts",
         RangeEnd = 181621
     },
     {
-        Id = 181622,
+        ID = 181622,
         Name = "Morningstar",
         RangeEnd = 181655
     },
     {
-        Id = 181656,
+        ID = 181656,
         Name = "Pawn",
         RangeEnd = 181689
     },
     {
-        Id = 181690,
+        ID = 181690,
         Name = "Double Lightning",
         RangeEnd = 181723
     },
     {
-        Id = 181724,
+        ID = 181724,
         Name = "Rose",
         RangeEnd = 181757
     },
     {
-        Id = 181758,
+        ID = 181758,
         Name = "Lion",
         RangeEnd = 181791
     },
     {
-        Id = 181792,
+        ID = 181792,
         Name = "Gnome",
         RangeEnd = 181825
     },
     {
-        Id = 181860,
+        ID = 181860,
         Name = "Evergreen Tree",
         RangeEnd = 181893
     },
     {
-        Id = 181894,
+        ID = 181894,
         Name = "Dynamite",
         RangeEnd = 181927
     },
     {
-        Id = 181928,
+        ID = 181928,
         Name = "Spider Web",
         RangeEnd = 181961
     },
     {
-        Id = 181962,
+        ID = 181962,
         Name = "Spider",
         RangeEnd = 181995
     },
     {
-        Id = 181996,
+        ID = 181996,
         Name = "Snowflake",
         RangeEnd = 182029
     },
     {
-        Id = 182030,
+        ID = 182030,
         Name = "Turtle",
         RangeEnd = 182063
     },
     {
-        Id = 182064,
+        ID = 182064,
         Name = "Beast",
         RangeEnd = 182097
     },
     {
-        Id = 182098,
+        ID = 182098,
         Name = "Quill",
         RangeEnd = 182131
     },
     {
-        Id = 182132,
+        ID = 182132,
         Name = "Rogue",
         RangeEnd = 182165
     },
     {
-        Id = 182166, -- 140
+        ID = 182166, -- 140
         Name = "Bird Foot",
         RangeEnd = 182199
     },
     {
-        Id = 182234,
+        ID = 182234,
         Name = "Snail Face",
         RangeEnd = 182267
     },
     {
-        Id = 182268,
+        ID = 182268,
         Name = "Entwined Serpents",
         RangeEnd = 182301
     },
     {
-        Id = 182302,
+        ID = 182302,
         Name = "Pirate Hat",
         RangeEnd = 182335
     },
     {
-        Id = 182336,
+        ID = 182336,
         Name = "Scroll",
         RangeEnd = 182369
     },
     {
-        Id = 182370,
+        ID = 182370,
         Name = "Crossed Pickaxes",
         RangeEnd = 182403
     },
     {
-        Id = 182404,
+        ID = 182404,
         Name = "Bunny Face",
         RangeEnd = 182437
     },
     {
-        Id = 182438,
+        ID = 182438,
         Name = "Frog",
         RangeEnd = 182471
     },
     {
-        Id = 182472,
+        ID = 182472,
         Name = "Foot",
         RangeEnd = 182505
     },
     {
-        Id = 182506,
+        ID = 182506,
         Name = "Barrel",
         RangeEnd = 182539
     },
     {
-        Id = 182540,
+        ID = 182540,
         Name = "Mushroom",
         RangeEnd = 182573
     },
     {
-        Id = 182608,
+        ID = 182608,
         Name = "Omega",
         RangeEnd = 182641
     },
     {
-        Id = 182642,
+        ID = 182642,
         Name = "Horseshoe",
         RangeEnd = 182675
     },
     {
-        Id = 182676,
+        ID = 182676,
         Name = "Potion",
         RangeEnd = 182709
     },
     {
-        Id = 182710,
+        ID = 182710,
         Name = "Hoof",
         RangeEnd = 182743
     },
     {
-        Id = 182744,
+        ID = 182744,
         Name = "Starfish",
         RangeEnd = 182777
     },
     {
-        Id = 182778,
+        ID = 182778,
         Name = "Weird Flowers",
         RangeEnd = 182811
     },
     {
-        Id = 182812,
+        ID = 182812,
         Name = "Intricate Design",
         RangeEnd = 182845
     },
     {
-        Id = 182846,
+        ID = 182846,
         Name = "Sad Mask",
         RangeEnd = 182879
     },
     {
-        Id = 182880,
+        ID = 182880,
         Name = "Lily",
         RangeEnd = 182913
     },
     {
-        Id = 182914, -- 160
+        ID = 182914, -- 160
         Name = "Viking Heart",
         RangeEnd = 182947
     },
     {
-        Id = 182982,
+        ID = 182982,
         Name = "Meso Face 1",
         RangeEnd = 183015
     },
     {
-        Id = 183016,
+        ID = 183016,
         Name = "Meso Face 2",
         RangeEnd = 183049
     },
     {
-        Id = 183050,
+        ID = 183050,
         Name = "Meso Face 3",
         RangeEnd = 183083
     },
     {
-        Id = 183084,
+        ID = 183084,
         Name = "Horde-Like Symbol",
         RangeEnd = 183117
     },
     {
-        Id = 183118,
+        ID = 183118,
         Name = "Old Horde Symbol",
         RangeEnd = 183151
     },
     {
-        Id = 183152,
+        ID = 183152,
         Name = "Star Skull",
         RangeEnd = 183185
     },
     {
-        Id = 183186,
+        ID = 183186,
         Name = "Compass Rose 2",
         RangeEnd = 183219
     },
     {
-        Id = 183220,
+        ID = 183220,
         Name = "Rage Face",
         RangeEnd = 183253
     },
     {
-        Id = 183254,
+        ID = 183254,
         Name = "Bloodlust",
         RangeEnd = 183287
     },
     {
-        Id = 183288,
+        ID = 183288,
         Name = "Bull with Flags",
         RangeEnd = 183321
     },
     {
-        Id = 465224,
+        ID = 465224,
         Name = "Masked and Hooded",
         RangeEnd = 465257
     },
     {
-        Id = 465258,
+        ID = 465258,
         Name = "Trinity Knot",
         RangeEnd = 465291
     },
     {
-        Id = 465292,
+        ID = 465292,
         Name = "Twelve Points",
         RangeEnd = 465325
     },
     {
-        Id = 465326,
+        ID = 465326,
         Name = "Phoenix",
         RangeEnd = 465359
     },
     {
-        Id = 465360, -- 175
+        ID = 465360, -- 175
         Name = "Goblin Face",
         RangeEnd = 465393
     },
     {
-        Id = 465394,
+        ID = 465394,
         Name = "Sceptre",
         RangeEnd = 465427
     },
     {
-        Id = 465428,
+        ID = 465428,
         Name = "Trophy",
         RangeEnd = 465461
     },
     {
-        Id = 465462,
+        ID = 465462,
         Name = "Terminator",
         RangeEnd = 465495
     },
     {
-        Id = 465496,
+        ID = 465496,
         Name = "Fruit",
         RangeEnd = 465529
     },
     {
-        Id = 465530,
+        ID = 465530,
         Name = "Rainbow",
         RangeEnd = 465563
     },
     {
-        Id = 465564,
+        ID = 465564,
         Name = "Intricate Turtle",
         RangeEnd = 465597
     },
     {
-        Id = 465598,
+        ID = 465598,
         Name = "Two Wolves Howling",
         RangeEnd = 465631
     },
     {
-        Id = 465632,
+        ID = 465632,
         Name = "Candle Without Holder",
         RangeEnd = 465665
     },
     {
-        Id = 465666,
+        ID = 465666,
         Name = "Octogon",
         RangeEnd = 465699
     },
     {
-        Id = 465700,
+        ID = 465700,
         Name = "Rook",
         RangeEnd = 465733
     },
     {
-        Id = 465734,
+        ID = 465734,
         Name = "Present",
         RangeEnd = 465767
     },
     {
-        Id = 465768,
+        ID = 465768,
         Name = "Gingerbread Man",
         RangeEnd = 465801
     },
     {
-        Id = 465802,
+        ID = 465802,
         Name = "Murloc",
         RangeEnd = 465835
     },
     {
-        Id = 467279,
+        ID = 467279,
         Name = "Overflowing Tankard",
         RangeEnd = 467312
     },
     {
-        Id = 467313,
+        ID = 467313,
         Name = "Wings",
         RangeEnd = 467346
     },
     {
-        Id = 467347,
+        ID = 467347,
         Name = "Claw Marks",
         RangeEnd = 467380
     },
     {
-        Id = 467381,
+        ID = 467381,
         Name = "Whip",
         RangeEnd = 467414
     },
     {
-        Id = 467415,
+        ID = 467415,
         Name = "Pointy Skull",
         RangeEnd = 467448
     },
     {
-        Id = 467449,
+        ID = 467449,
         Name = "Wolf Face",
         RangeEnd = 467482
     },
     {
-        Id = 467483,
+        ID = 467483,
         Name = "Even More Arcane Eye",
         RangeEnd = 467516
     },
     {
-        Id = 467517,
+        ID = 467517,
         Name = "Abstract",
         RangeEnd = 467550
     }
